b2a069950036ba4ecfe422f2954dbc697a557c16
foenyxr <pranjalmlokhande@gmail.com>
2024-10-16 06:00:52 -0400

    added fixed files

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // Replace with your frontend's URL
+  credentials: true, // If you need to send cookies or authentication headers
+}));
+
 app.use(express.json());
 
 // const corsOptions = {
